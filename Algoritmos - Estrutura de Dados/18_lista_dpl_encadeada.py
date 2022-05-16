c6c5d8e6728a715901d8b5cafe81f8d87991abee
Viniciusgsc <88463035+Viniciusgsc@users.noreply.github.com>
2022-05-16 20:28:50 -0300

    "(Adição de Métodos na Lista Duplamente Encadeada)"
    "(Adição de Métodos na Lista Duplamente Encadeada)"

---
+++ Algoritmos - Estrutura de Dados/18_lista_dpl_encadeada.py
@@ -0,0 +1,24 @@
+from lib.doubly_linked_list import DoublyLinkedList
+
+lista = DoublyLinkedList()
+print(lista)
+
+#Inserção em lista vazia; a posição será ignorada
+lista.insert(5, 'Epaminondas')
+print(lista)
+
+#Inserção no inicio da lista
+lista.insert_head('Filisberto') # Equivale a: lista.insert(0, 'Filibersto)
+print(lista)
+
+#Inserção no final da lista
+lista.insert_tail('Jeruza') #Equivale a: lista.insert(lista.count,'Jeruza')
+print(lista)
+
+#Inserção em posição intermediária
+lista.insert(2,'Laudicéia')
+print(lista)
+lista.insert(1,'Drusila')
+print(lista)
+
+
